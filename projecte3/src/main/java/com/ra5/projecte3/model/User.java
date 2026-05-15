9b18cee17f0d9fd56fbb2f7dc071bb2499bfad65
omar <omar@>
2026-05-15 10:55:26 +0200

    Issue 8 Implementar UserService

--- projecte3/src/main/java/com/ra5/projecte3/model/User.java
+++ projecte3/src/main/java/com/ra5/projecte3/model/User.java
@@ -1,6 +1,7 @@
 package com.ra5.projecte3.model;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -112,6 +113,7 @@ public class User {
     public void setAcademicProfile(AcademicProfile academicProfile) {
         this.academicProfile = academicProfile;
     }
+
 
     
 
